a87a9f27e50f66136c4fb8b95babc7319a2552d2
ekaterina <kica111@mail.ru>
2022-08-22 19:35:08 +0800

    Решена задача 2

--- reshenie02/Program.cs
+++ reshenie02/Program.cs
@@ -9,3 +9,27 @@ Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max) 
+{
+    max = numberB;
+    if (max > numberC) 
+    {
+        System.Console.WriteLine(numberB+" максимальное число");
+    }
+    else
+    {
+        System.Console.WriteLine(numberC+" максимальное число");
+    }
+}
+else
+{
+    if (numberC > max) 
+    {
+        System.Console.WriteLine(numberC+" максимальное число");
+    }
+    else
+    {
+        System.Console.WriteLine(numberA+" максимальное число");
+    }
+}
